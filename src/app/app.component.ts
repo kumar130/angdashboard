50d079f0ebe640d1ddfb89ff720441c844102712
saurabhkumar <saurav.tech812@gmail.com>
2026-02-23 11:56:50 +0530

    changes -2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,43 +1,10 @@
 import { Component } from '@angular/core';
-
-interface TagRule {
-  key: string;
-  value: string;
-}
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html'
 })
-export class AppComponent {
-
-  step = 1;
-
-  tagKey = '';
-  tagValue = '';
-
-  rules: TagRule[] = [];
-
-  // Step navigation
-  getStarted() {
-    this.step = 2;
-  }
-
-  configureTagPolicy() {
-    this.step = 3;
-  }
-
-  addRule() {
-    if (!this.tagKey || !this.tagValue) return;
-
-    this.rules.push({
-      key: this.tagKey,
-      value: this.tagValue
-    });
-
-    // reset fields
-    this.tagKey = '';
-    this.tagValue = '';
-  }
-}
+export class AppComponent {}
