770374d5c2a553767f25d21614e06dfbdf6dd394
saurabhkumar <saurav.tech812@gmail.com>
2026-02-23 12:30:50 +0530

    adding a file

---
+++ src/app/services/tag-config.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+ providedIn: 'root'
+})
+export class TagConfigService {
+
+  private rules: any[] = [];
+
+  setRules(rules: any[]) {
+    this.rules = rules;
+  }
+
+  getRules() {
+    return this.rules;
+  }
+}
