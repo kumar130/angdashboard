57fa5dd605e385bf783d762c5ebfcb746d3239e0
saurabhkumar <saurav.tech812@gmail.com>
2026-02-23 16:23:33 +0530

    failurefix

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,16 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideRouter } from '@angular/router';
+import { importProvidersFrom } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { provideHttpClient } from '@angular/common/http';
-import { provideForms } from '@angular/forms';
 
 import { AppComponent } from './app/app.component';
-import { routes } from './app/app.routes';
 
 bootstrapApplication(AppComponent, {
   providers: [
-    provideRouter(routes),
     provideHttpClient(),
-    provideForms()
+    importProvidersFrom(
+      FormsModule,
+      ReactiveFormsModule
+    )
   ]
 }).catch(err => console.error(err));
